54fdcb7e25431b53c2fee5f4f2a8dc86560a9b80
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2023-04-28 23:00:20 -0600

    Se edito la tabla persona y se hizo un respaldo de la base de datos

--- SQLQueryBDPrueba.sql
+++ SQLQueryBDPrueba.sql
@@ -21,10 +21,11 @@ create table Persona(
 	, primerApellido nvarchar(50) NOT NULL
 	, segundoApellido nvarchar(50) 
 	, fechaNac dateTime NOT NULL
-	, sexo bit default 1 NOT NULL
-	, telefono nvarchar(16) NOT NULL
+	, sexo bit default 1 
+	, telefono nvarchar(16) 
 	, email nvarchar(100)
-	, direccion nvarchar(100) NOT NULL
+	,idCiudad int
+	, direccion nvarchar(100) 
 	, estado bit default 'true'
 
 );
@@ -36,6 +37,64 @@ go
 Insert into Ciudad(nombre) values(N'Masaya') 
 Insert into Ciudad(nombre) values(N'Managua'),(N'Granada'),(N'Rivas'),(N'León')
 
+
 --Mostrar Registro
 
 Select * from Ciudad
+
+
+--Editar Registros
+Update Ciudad set nombre = N'Leon', estado = 0 where id = 4
+
+/*En los valores boleanos se puede usar el uno como el cero o el true o el false*/
+Update Ciudad set nombre = N'Leon', estado = 'True' where id = 4
+go
+
+--Eliminar Registros
+delete from Ciudad where id = 4
+go
+
+--Antes de tocar una base de datos para mantenimiento hay que hacer respaldos
+
+--Si hago esto se eliminan todos los datos de la base de datos, ya que no especifico una fila en especifico
+
+delete from Ciudad 
+go
+
+insert into Ciudad(nombre) values (N'Rivas')
+go
+
+--si hago esto se escribe en toda la base de datos
+update Ciudad set nombre = N'Rivas'
+
+--Insertar registros a la tabla Personas 
+Insert into Persona(primerNombre, segundoNombre, primerApellido, segundoApellido, fechaNac, idCiudad) values (N'Auxiliadora', N'Marcela', N'Garcia', N'Serrano', '22/02/1974', 1)
+go
+
+Insert into Persona(primerNombre, segundoNombre, primerApellido, segundoApellido, fechaNac, idCiudad) values (N'María', N'Paula', N'Garcia', N'Lopez', '07/06/2005', 1)
+go
+
+Insert into Persona(primerNombre, segundoNombre, primerApellido, segundoApellido, fechaNac, idCiudad) values (N'Flavio', N'Moises', N'Arana', N'Garcia', '31/12/1992', 1)
+go
+
+Select * from Persona
+go
+--consultas de seccion 
+--esto lo cree utilizando ctr shift q
+SELECT primerNombre, primerApellido, fechaNac, idCiudad
+FROM     Persona
+
+--mostrar en una coluna el primer nombre y el primer apellido y en otra columna la edad de la persona
+
+SELECT primerNombre+N' '+primerApellido as N'Nombre Completo', year(getdate())-year(fechaNac) as Edad, idCiudad
+FROM     Persona
+
+print(GetDate()) --imprime la hora a la que se ejecuta el programa
+
+print(year(getDate())) --solo imprime el anio de la fecha
+print Month(GetDate())  --muestra unicamente el numero del mes
+print Day(GetDate()) --muestra el dia
+
+print convert(nvarchar(10), GetDate(), 102)--muestra la fecha en el formato britanico según el profesor Duran
+
+/*102 - 103 foramato británico*/ 
